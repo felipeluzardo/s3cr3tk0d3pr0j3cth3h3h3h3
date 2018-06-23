49ff1dff4d0e6785e085e8b32451cfd8b1dd6f9f
Felipe Luzardo <felipeluzardo@outlook.com>
2018-06-23 15:23:06 +0300

    testing

---
+++ HorrorStealthGame-desktop/src/feli/hsg/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package feli.hsg.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import feli.hsg.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.foregroundFPS = 60;
+		new LwjglApplication(new MainGame(), config);
+	}
+}
